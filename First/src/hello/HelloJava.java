342ebfadaeb7c7d4fbe431c2ad777ecb5de94982
jangbigom91 <jangbigom91@gmail.com>
2021-11-15 11:01:50 +0900

    11/15 hello, java

---
+++ First/src/hello/HelloJava.java
@@ -0,0 +1,15 @@
+/* 
+  날짜 : 2021년 11월 15일
+  이름 : 최정한
+  내용 : 자바 첫 시작
+ */
+package hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+
+	}
+
+}
